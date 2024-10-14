070bef5f80651d45d1511e45ebf0b4b6cff2e3ff
a-njali-raj <anjalirraj@gmail.com>
2024-10-14 10:03:29 +0530

    Angular initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RegisterComponent,NavbarComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  
 })
 export class AppComponent {
   title = 'Angularsample';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+
+export const routes: Routes = [
+    {
+        path:'register' ,component:RegisterComponent
+        
+    },
+    {
+        path:'navbar',component:NavbarComponent
+    },
+    {
+        path:'login',component:LoginComponent
+    }
+];
